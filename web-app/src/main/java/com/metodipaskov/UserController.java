7a33b1a7db8f7fde57c088b2a3924f89a5d5ae19
paskovm <metodi_paskov@yahoo.com>
2025-01-03 15:34:46 +0200

    module with dependencies from another modules

---
+++ web-app/src/main/java/com/metodipaskov/UserController.java
@@ -0,0 +1,11 @@
+package com.metodipaskov;
+
+import com.metodipaskov.domain.UserCommand;
+
+public class UserController {
+
+    User saveUser(UserCommand userCommand) {
+        // fake impl
+        return new User();
+    }
+}
